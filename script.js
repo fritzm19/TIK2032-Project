691f291e87c7a4eb2fa64a5044b641c00f25ff66
Fritz Manambe <manambefritz19@gmail.com>
2024-05-21 19:09:48 +0800

    Adding alert on script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-<script>
-  // Your JavaScript code here
-  function greetUser() {
-    alert("Hello from inline JavaScript!");
-  }
-</script>
+// Display a welcome alert when the page loads
+///*
+window.onload = function () {
+	alert("Welcome to my personal homepage!");
+};
+
+// Add functionality to a button (if you add one in the HTML)
+/*
+document.addEventListener("DOMContentLoaded", (event) => {
+	const button = document.createElement("button");
+	button.textContent = "Click Me!";
+	document.body.appendChild(button);
+
+	button.addEventListener("click", function () {
+		alert("Button was clicked!");
+	});
+});
+*/
